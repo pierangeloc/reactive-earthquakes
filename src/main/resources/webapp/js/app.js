e2b59519ad503b31f53c4660af2d32f359ecd2a7
pierangeloc <pierangeloc@gmail.com>
2015-11-06 10:22:55 +0100

    added log area TODO: - add events details on markers - extract some stats on server side

--- src/main/resources/webapp/js/app.js
+++ src/main/resources/webapp/js/app.js
@@ -9,7 +9,20 @@ var initialize = function(map) {
         data: eventsArray
     });
 
+    var eventLog = [];
+
     heatmap.setMap(map);
+
+    var RETENTION = 100;
+    var appendEvent = function(evt) {
+        eventLog.unshift( (new Date(parseInt(evt.time))).toUTCString() + '-' +
+                        evt.place + '-' +
+                        evt.magnitude + '-');
+        eventLog = eventLog.slice(0, RETENTION);
+        document.getElementById('timeline').value = eventLog.join('\n');
+
+
+    }
 
     events.onopen = function(evt) {
         console.log("ws connection accepted");
@@ -17,14 +30,41 @@ var initialize = function(map) {
 
     events.onmessage = function(msg){
         var event = JSON.parse(msg.data);
-        console.log("event occurred: " + JSON.stringify(event));
+        console.log('event occurred: ' + JSON.stringify(event));
+        appendEvent(event);
 
         eventsArray.push({location: new google.maps.LatLng(event.lat, event.long), weight: Math.ceil(event.magnitude)});
-        //var marker = new google.maps.Marker({
-        //    map: map,
-        //    draggable: true,
-        //    animation: google.maps.Animation.DROP,
-        //    position: {lat: event.lat, lng: event.long}
-        //});
+
+        //items.push({id: 7, content: 'item 7', start: '2015-04-27'})
+        //let's drop also a marker that will vanish after a while:
+        var marker = new google.maps.Marker({
+            map: map,
+            icon: getCircle(event.magnitude),
+            draggable: false,
+            animation: google.maps.Animation.DROP,
+            position: {lat: event.lat, lng: event.long}
+        });
+        window.setTimeout(function() {
+            removeMarker(marker);
+        }, 10000);
     };
+
+    var removeMarker = function(marker) {
+       marker.setMap(null);
+    };
+
+    /**
+     * See https://developers.google.com/maps/tutorials/visualizing/earthquakes
+     */
+    function getCircle(magnitude) {
+        var circle = {
+            path: google.maps.SymbolPath.CIRCLE,
+            fillColor: 'red',
+            fillOpacity: .5,
+            scale: Math.pow(2, magnitude),
+            strokeColor: 'white',
+            strokeWeight: .7
+        };
+        return circle;
+    }
 };
